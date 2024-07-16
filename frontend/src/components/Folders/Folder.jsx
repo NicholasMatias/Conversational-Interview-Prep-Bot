b71576de83b2b6cab8322cb4dd22ba73d2d89ab5
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-07-15 22:38:11 -0700

    Modal created for folders on folder page.

--- frontend/src/components/Folders/Folder.jsx
+++ frontend/src/components/Folders/Folder.jsx
@@ -1,13 +1,48 @@
+import { useState } from 'react'
 import './Folder.css'
 
 
 
-const Folder = ({folderName}) => {
+const Folder = ({ folderName }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [transcripts, setTranscripts] = useState([]);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+        
+    }
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+
+
+    const openFolder = () => {
+
+    }
+
     return (
-        <div className='folder'>
-            <i className="fa-solid fa-folder"></i>
-            <h3 className='folder-name'>{folderName}</h3>
-        </div>
+        <>
+            <div className='folder' onClick={handleOpenModal}>
+                <i className="fa-solid fa-folder"></i>
+                <h3 className='folder-name'>{folderName}</h3>
+            </div>
+
+            {isModalOpen &&
+                <div className='overlay'>
+                    <div className='folder-modal-container'>
+                        <h1>
+                            {`${folderName}'s Transcripts`}
+                        </h1>
+                        <button onClick={(() => setIsModalOpen(false))}>Close</button>
+                    </div>
+
+                </div>
+
+
+            }
+
+        </>
     )
 }
 
