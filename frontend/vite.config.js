5ce4c48b6488c2d34d97e9830f8110873a30e65c
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-07-31 16:34:16 -0700

    test => no external

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    rollupOptions: {
-      external: ['firebase','firebase/app', 'firebase/auth','firebase/firestore']
-    }
-  },
+  
 
   plugins: [react()],
 
