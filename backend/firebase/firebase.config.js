bc0178fefe7cd107d4d90e4ae25255077bf03688
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-07-31 15:03:25 -0700

    testing deployment

--- backend/firebase/firebase.config.js
+++ backend/firebase/firebase.config.js
@@ -1,5 +1,5 @@
 // import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import {
     getAuth,
     signInWithEmailAndPassword,
@@ -41,7 +41,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
