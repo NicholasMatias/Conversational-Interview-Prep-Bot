d037ee10bc1df85f48d386304a7779eddc0b2764
Nicholas Matias <113799954+NicholasMatias@users.noreply.github.com>
2024-07-31 15:23:12 -0700

    removed ./

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build:{
     rollupOptions: {
-      external: ['./firebase/app', './firebase/auth','./firebase/firestore']
+      external: ['firebase/app', 'firebase/auth','firebase/firestore']
     }
   },
 
